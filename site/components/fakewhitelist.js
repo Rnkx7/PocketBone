3fc60c7b1414f89c19cb38e0709b04018ea5a97e
weedenrj <70019843+weedenrj@users.noreply.github.com>
2022-01-06 16:47:19 -0600

    added new address
    dfg

--- site/components/fakewhitelist.js
+++ site/components/fakewhitelist.js
@@ -3,5 +3,5 @@ export const fakeWhitelist = [
   "0x98eAfb43BB4688d12C561eda173fCe11e7d2c911".toLowerCase(),
   "0xdb734624D5f1F53508121684d222e5393fAB2843".toLowerCase(),
   "0xC4AA5efbbdFaEe70A94C48bdF82450C29EE9aEc5".toLowerCase(),
-  "0x7f391BAD72ABde3A6c7eDBd60aaEd2c002Ac61e8".toLowerCase()
+  "0x7f391BAD72ABde3A6c7eDBd60aaEd2c002Ac61e8".toLowerCase(),
 ];
